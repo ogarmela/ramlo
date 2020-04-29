c36352a0d48f25e9ac9982fe00ae5be932887413
ogarmela <52357639+ogarmela@users.noreply.github.com>
2020-04-28 18:48:55 -0700

    Add files via upload

---
+++ salmo1.java
@@ -0,0 +1,18 @@
+
+public class salmo1 {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < array.size(); i++) {
+	        String word = array.get(i);
+	        if (word.length() % 2 == 0) {
+	            array.remove(i);
+	            i--;
+
+	}
+
+}
+		
+	}
+	
+}
+	
